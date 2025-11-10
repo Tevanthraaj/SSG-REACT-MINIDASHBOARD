0a63b72bd0ee301355cbc0104f19c8ccfb4116e7
Tevanthraaj <tevanthraajofficial@gmail.com>
2025-11-10 10:07:48 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard/>}/>
